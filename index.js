a7320382195e20e28ae60e9b4e99bad0e55ba4e6
Tanvir Ahmed <tanvir.be.global@gmail.com>
2022-10-12 14:52:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+// Mongodb Server 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://super-assistant:admin321@cluster0.x3bqwpe.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        // Connect the client to the server (optional starting in v4.7)
+        await client.connect();
+        // Establish and verify connection  
+        await client.db("admin").command({ ping: 1 });
+        console.log("Connected successfully to server");
+
+        const questionsCollection = client.db("questions").collection("question");
+
+
+        // Create Api >>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir)
+
+
+
+//Root Get api
+app.get('/', (req, res) => {
+    res.send("Super-Assistant Server is Runnin...")
+})
+
+app.listen(port, () => {
+    console.log('listen to port, ', port);
+})
+
+//user_name: super-assistant
+//user_password: admin321
